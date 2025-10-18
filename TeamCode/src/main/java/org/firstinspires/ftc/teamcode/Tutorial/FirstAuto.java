693bab391a795e599b8ea4ebd7b479b227985490
Team 5233 Vector <vector5233@gmail.com>
2025-10-18 09:58:01 -0400

    Added in MeemMeep

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tutorial/FirstAuto.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.Tutorial;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+
+@Autonomous(name="FirstAuto")
+public class FirstAuto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // creating starting pose:
+        Pose2d beginPose = new Pose2d(new Vector2d(0, 63), Math.toRadians(270));
+
+        // creating RR Drive object
+        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+        waitForStart();
+
+        Action path = drive.actionBuilder(beginPose)
+                .splineToSplineHeading(new Pose2d(40,0,Math.toRadians(180)), Math.toRadians(270))
+                .splineToConstantHeading(new Vector2d(40,-48), Math.toRadians(0))
+                .lineToXConstantHeading(0)
+                .build();
+
+                Actions.runBlocking(new SequentialAction(path));
+
+    }
+}
