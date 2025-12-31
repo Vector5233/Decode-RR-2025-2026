a93d19486cc4a18de57f5aabd000ecb9cdb37c86
Kirk Frankfurth <kfrankfurth@chapelgateacademy.org>
2025-12-31 10:46:51 -0500

    This is the TELEOPV2 File

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl/TELEOPV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl/TELEOPV2.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.DriverControl;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 /**Configuration file
  * Drive train motors will called - we many want to make a diveMotor class later - Chad this would be your input
  * motor names in the configuration:  on Robot Controller: this is where you name the motors in the app
@@ -10,10 +12,26 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
  * frontRightMotor
  * backLeftMotor
  * backRightMotor
+ * flyWheel
+ * collector
+ * leftLift
+ * rightLift
+ * flpperOne
+ * flipperTwo
  */
 @Disabled
 @TeleOp(group = "Primary", name = "Basic TeleOp Structure")
 public class TELEOPV2 extends LinearOpMode {
+    //declare motors
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    //collector and shooter motors can be declared here when needed
+    private DcMotor flyWheel;
+    private DcMotor collector;
+
     @Override
     public void  runOpMode() throws InterruptedException{
         //method to initialize hardware
@@ -28,9 +46,24 @@ public class TELEOPV2 extends LinearOpMode {
         while(opModeIsActive()){
             //code to run while op mode is active
             //Add telemetry here if needed
+
         }
     }
     public void initHardware(){
+
         //code to initialize hardware
     }
+
+    public void initDriveTrain(){
+
+
+
+    }
+
+    public void teleoOpControls(){
+        //mechanum drive code here for controls
+
+
+
+
 }
