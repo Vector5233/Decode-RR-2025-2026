87f836fe149042598542a9d7a8e351af0e34cff8
Team 5233 Vector <vector5233@gmail.com>
2026-04-02 10:21:00 -0400

    It works

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -2,11 +2,9 @@ package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
-import com.noahbres.meepmeep.MeepMeep;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
@@ -14,14 +12,19 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 18)
                 .build();
 
-        Action path = myBot.getDrive().actionBuilder(new Pose2d(0, 67, 0))
-                .splineToSplineHeading(new Pose2d(40,0,Math.toRadians(270)), Math.toRadians(270))
-                .splineToConstantHeading(new Vector2d(40,-48), Math.toRadians(180))
-                .lineToXConstantHeading(0)
-                .build();
+       Action path = myBot.getDrive().actionBuilder(new Pose2d(62, 0, -163))
+                        .lineToX(38)
+                        .turn(Math.toRadians(-90))
+                        .lineToY(33)
+                        .build();
+//        Action path = myBot.getDrive().actionBuilder(new Pose2d(0, 67, 0))
+//                .splineToSplineHeading(new Pose2d(40,0,Math.toRadians(270)), Math.toRadians(270))
+//                .splineToConstantHeading(new Vector2d(40,-48), Math.toRadians(180))
+//                .lineToXConstantHeading(0)
+//                .build();
 
         myBot.runAction(path);
 
